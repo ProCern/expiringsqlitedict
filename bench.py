58b5b1216b1e2c7da59bf2192ba29a0ad2ecb2cd
Taylor C. Richberger <taywee@gmx.com>
2023-01-17 11:23:52 -0700

    add simple benchmark to help compare for future performance changes

---
+++ bench.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from datetime import timedelta
+import random
+import string
+from timeit import Timer
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from expiringsqlitedict import SqliteDict
+import gc
+population = string.ascii_letters + string.digits + string.punctuation
+
+with TemporaryDirectory() as dir:
+    db_file = Path(dir) / 'test.db'
+
+    def test():
+        with SqliteDict(str(db_file), lifespan=timedelta(seconds=1)) as db:
+            k = ''.join(random.choices(population, k=10))
+            v = ''.join(random.choices(population, k=10))
+            db[k] = v
+    
+
+    timer = Timer('test()', globals={'test': test})
+
+    runs = 0
+    time = 0
+
+    # Run for at least 5 seconds
+    for second in range(5):
+        for fraction in range(5):
+            part_runs, part_time = timer.autorange()
+            runs += part_runs
+            time += part_time
+
+    size = db_file.stat().st_size
+
+    print(f'{runs=} / {time=} = {runs / time=}')
+    print(f'{size=}')
+

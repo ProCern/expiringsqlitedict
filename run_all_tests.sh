122f17b092c7674a2778cce3e98cccf3341a7953
Taylor C. Richberger <tcr@absolute-performance.com>
2023-05-01 09:43:56 -0600

    add centos 7 test for very old versions of things

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,11 +2,9 @@
 
 set -euf
 
-podman=$(command -v podman || command -v docker)
-
 for version in 3.6 3.7 3.8 3.9 3.10 3.11; do
   echo $version
-  "$podman" container run \
+  podman container run \
     --rm \
     -e PYTHONPATH=/app/src \
     --mount type=bind,source=.,destination=/app,ro=true \
@@ -17,8 +15,23 @@ for version in 3.6 3.7 3.8 3.9 3.10 3.11; do
     python3 -munittest "/app/test.py"
 done
 
+echo centos:7
+podman container run \
+  --rm \
+  -e PYTHONPATH=/app/src \
+  --mount type=bind,source=.,destination=/app,ro=true \
+  --security-opt label=disable \
+  centos:7 \
+  /bin/sh -c '
+  set -euvf
+
+  yum update -y
+  yum install -y python3 sqlite3
+  python3 -munittest "/app/test.py"
+'
+
 echo fedora:38
-"$podman" container run \
+podman container run \
   --rm \
   -e PYTHONPATH=/app/src \
   --mount type=bind,source=.,destination=/app,ro=true \
